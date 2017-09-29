d005ba3e6980261cdb8d18769026a781ecba9a63
renjiajian <renjiajian@withub.net.cn>
2017-09-29 23:52:54 +0800

    Initial commit

---
+++ app/src/main/java/com/ivotai/kotlindemo/MainActivity.kt
@@ -0,0 +1,9 @@
+package com.ivotai.kotlindemo
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+
+}
---
+++ app/src/test/java/com/ivotai/kotlindemo/ExampleUnitTest.kt
@@ -0,0 +1,25 @@
+package com.ivotai.kotlindemo
+
+import io.kotlintest.Spec
+import io.kotlintest.matchers.shouldBe
+import io.kotlintest.specs.FunSpec
+import org.junit.Test
+
+import org.junit.Assert.*
+
+/**
+ * Example local unit test, which will execute on the development machine (host).
+ *
+ * @see [Testing documentation](http://d.android.com/tools/testing)
+ */
+class ExampleUnitTest {
+    @Test
+    @Throws(Exception::class)
+    fun addition_isCorrect() {
+        assertEquals(4, (2 + 2).toLong())
+
+
+    }
+
+
+}
